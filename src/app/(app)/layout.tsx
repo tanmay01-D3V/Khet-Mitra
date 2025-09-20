de3d7c01226902264e582fac39fa538f55dda4b8
Saad Mulla ! <shirinmulla99@gmail.com>
2025-09-20 18:18:20 +0000

    YOU HAVENT REMOVED THE BLANK SPACE LEFT ABOVE THE CONTENT , I DONT NEED

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -38,7 +38,7 @@ export default function AppLayout({
               </Sidebar>
               <div className="flex flex-col sm:pl-14">
                   <Header />
-                  <main className="flex-1 items-start gap-4 p-4 sm:px-6 md:gap-8">
+                  <main className="gap-4 p-4 sm:px-6 md:gap-8">
                     {children}
                   </main>
               </div>
