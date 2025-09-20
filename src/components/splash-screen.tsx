e60b5c3f50736249cafd3f6f64e4f87f39f044bb
Saad Mulla ! <shirinmulla99@gmail.com>
2025-09-20 19:29:18 +0000

    give the loading logo an animation window of 1.5 seconds

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -4,7 +4,7 @@ import { Logo } from './logo';
 export function SplashScreen() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-background">
-      <div className="animate-in fade-in zoom-in-75 duration-1000">
+      <div className="animate-in fade-in zoom-in-75 duration-[1500ms]">
         <Logo className="h-24 w-auto" />
       </div>
     </div>
