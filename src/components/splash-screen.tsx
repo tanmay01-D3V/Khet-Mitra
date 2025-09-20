ac907bdc02e651979cc7595b6f36e0db9a37b855
Saad Mulla ! <shirinmulla99@gmail.com>
2025-09-20 19:32:39 +0000

    the logo animation should be in blavk color , not to white colour

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -3,7 +3,7 @@ import { Logo } from './logo';
 
 export function SplashScreen() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-background">
+    <div className="flex h-screen w-full items-center justify-center bg-black">
       <div className="animate-splash-in-out">
         <Logo className="h-24 w-auto" />
       </div>
